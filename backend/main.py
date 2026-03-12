98e86327c1ff995a84fb54bf81bd310795d7de06
launchpad <launchpad@steadyworks.ai>
2026-03-11 21:02:17 -0700

    base

---
+++ backend/main.py
@@ -0,0 +1,9 @@
+import json
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class Handler(BaseHTTPRequestHandler):
+    pass
+
+
+HTTPServer(("0.0.0.0", 3001), Handler).serve_forever()
